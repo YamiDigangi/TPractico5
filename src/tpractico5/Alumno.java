e0c4c59affbd1dcda3062222760e2e9fb7161d32
YamiDigangi <112526959+YamiDigangi@users.noreply.github.com>
2022-09-12 13:30:11 -0300

    Initial commit

---
+++ src/tpractico5/Alumno.java
@@ -0,0 +1,54 @@
+
+package tpractico5;
+
+
+public class Alumno {
+    
+    private int legajo;
+    private String apellido;
+    private String nombre;
+
+    public Alumno() {
+    }
+    
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void agregarMateria(Materia materia){
+        
+    }
+    
+    public int cantidadMaterias(){
+        return 0; 
+    }
+    
+    
+}
